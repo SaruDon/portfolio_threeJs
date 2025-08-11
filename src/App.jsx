85b5def0de3852a5c90cf3662b158a660b7480f3
Sarvesh Khamkar <sarveshkhamkar321@gmail.com>
2025-08-11 14:05:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-3xl text-white">Hello Three Js</h1>
+    </main>
+  );
+};
+
+export default App;
